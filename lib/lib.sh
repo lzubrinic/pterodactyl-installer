b10942a6b28bc9f2aa14cf3bd683c0162e03c409
Luka Žubrinić <lzubrinic20@student.foi.hr>
2024-08-30 22:40:24 +0200

    Update lib.sh

--- lib/lib.sh
+++ lib/lib.sh
@@ -47,12 +47,11 @@ export OS_VER_MAJOR=""
 export CPU_ARCHITECTURE=""
 export ARCH=""
 export SUPPORTED=false
-
 # download URLs
-export PANEL_DL_URL="https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz"
+export PANEL_DL_URL="https://github.com/pterodactyl/panel/releases/download/v1.11.5/panel.tar.gz"
 export WINGS_DL_BASE_URL="https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_"
 export MARIADB_URL="https://downloads.mariadb.com/MariaDB/mariadb_repo_setup"
-export GITHUB_BASE_URL=${GITHUB_BASE_URL:-"https://raw.githubusercontent.com/pterodactyl-installer/pterodactyl-installer"}
+export GITHUB_BASE_URL=${GITHUB_BASE_URL:-"https://raw.githubusercontent.com/lzubrinic/pterodactyl-installer/"}
 export GITHUB_URL="$GITHUB_BASE_URL/$GITHUB_SOURCE"
 
 # Colors
